7b7555ba9cd303f8961e60619308f797761a4cee
Sean Moriarty <spmoriarty1@gmail.com>
2022-08-15 10:52:03 -0700

    going down model/controller to build out testing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,36 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
-describe('backend-express-template routes', () => {
+
+
+const mockUser = {
+  firstName: 'Charlie',
+  lastName: 'Brown',
+  email: 'peanuts@gang.com',
+  password: '12345',
+};
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
+
+
+  const agent = request.agent(app);
+
+
+  const user = await UserService.create({ ...mockUser, ...userProps });
+
+
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  return [agent, user];
+};
+
+
+
+describe('RESTfull route testing zone', () => {
   beforeEach(() => {
     return setup(pool);
   });
@@ -12,5 +39,17 @@ describe('backend-express-template routes', () => {
   });
   afterAll(() => {
     pool.end();
+  });
+
+  it('creates a new user', async () => {
+    const res = await request(app).post('/api/v1/users').send(mockUser);
+    const { firstName, lastName, email } = mockUser;
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      firstName,
+      lastName,
+      email,
+    });
   });
 });
