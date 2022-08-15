ba5e886661c1f6d4803303622f93e799683f9e68
Sean Moriarty <spmoriarty1@gmail.com>
2022-08-15 12:48:40 -0700

    built out restaurant test, controller and model

--- __tests__/eatery.test.js
+++ __tests__/eatery.test.js
@@ -10,8 +10,29 @@ describe('RESTfull route testing zone', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it.skip('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it.skip('get all eateries from table', async () => {
+    const res = await request(app).get('/restaurant');
+    // eslint-disable-next-line
+        const expected = [{
+      id: '1',
+      name: 'Taco Bell',
+      city: 'Portland',
+      address: '123 Gassy Lane'
+    },
+    {
+      id: '2',
+      name: 'McDonalds',
+      city: 'Gresham',
+      address: '456 McHappy St'
+    }];
+
+    expect(res.status).toBe(200);
+    expect(res.body[0]).toEqual({
+      id: '3',
+      name: 'Wendys',
+      city: 'Oswego',
+      address: '789 Distastefull Rd'
+    });
   });
   afterAll(() => {
     pool.end();
